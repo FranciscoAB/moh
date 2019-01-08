958bf9c75e8ba673ceffb16b2b3b412128ec872e
FranciscoAB <37817729+FranciscoAB@users.noreply.github.com>
2019-01-08 20:49:59 -0300

    Update PlayerMisc.py

--- PlayerMisc.py
+++ PlayerMisc.py
@@ -7,20 +7,20 @@ class Player(pygame.sprite.Sprite):
 
         self.image = pygame.image.load("player.png").convert_alpha()
         self.rect = self.image.get_rect()
+
     def moveUp(self):
         if self.rect.y > 0:
             self.rect.y -= 1        
     def moveDown(self):
-        if self.rect.y < DISPLAY_HEIGHT - self.rect.height:
+        if self.rect.y < DISPLAY_HEIGHT_GAMEZONE - self.rect.height:
             self.rect.y += 1
     def moveRight(self):
-        if self.rect.x < DISPLAY_WIDTH - self.rect.width:
+        if self.rect.x < DISPLAY_WIDTH_GAMEZONE - self.rect.width:
             self.rect.x += 1
     def moveLeft(self):
         if self.rect.x > 0:
             self.rect.x -= 1
             
-
 class PlayerBullet (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
