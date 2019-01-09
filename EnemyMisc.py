13bc0e0d38e11f1e72907451ea666c9a10f9858b
FranciscoAB <37817729+FranciscoAB@users.noreply.github.com>
2019-01-09 00:28:06 -0300

    Update EnemyMisc.py

--- EnemyMisc.py
+++ EnemyMisc.py
@@ -9,18 +9,16 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.image.load("enemy.png").convert_alpha()
         self.rect = self.image.get_rect()
 
-        self.timerBeforeMoveLimit = randint(1, 2)
-        self.timerBeforeMove = 0
+        self.updateTimerLimit = 2
+        self.updateTimer = 0
 
-    def update(self):
-        
-        self.timerBeforeMove += 1
-        
-        if self.timerBeforeMove >= self.timerBeforeMoveLimit:
-            self.timerBeforeMove = 0
-            self.rect.y += 1            
+    def update(self, deltaTime):
+        if self.updateTimer >= self.updateTimerLimit:
+            self.rect.y += 1
+            self.updateTimer = 0
+        self.updateTimer += deltaTime
         
         if self.rect.y > DISPLAY_HEIGHT_GAMEZONE:
             self.rect.x = randint(0, DISPLAY_WIDTH_GAMEZONE - self.rect.w)
-            self.timerBeforeMoveLimit = randint(1, 2)
             self.rect.y = 0
+            self.updateTimerLimit = randint(2, 10)
