c1a5d731206f8bde906487527e981f0097684e8d
FranciscoAB <37817729+FranciscoAB@users.noreply.github.com>
2019-01-09 00:29:45 -0300

    Update PlayerMisc.py

--- PlayerMisc.py
+++ PlayerMisc.py
@@ -1,39 +1,62 @@
 import pygame
 from Util import *
 
+'''
+Class representing the player.
+'''
 class Player(pygame.sprite.Sprite):
+    '''
+    Constructor
+    '''
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("player.png").convert_alpha()
         self.rect = self.image.get_rect()
 
+        self.updateTimerLimit = 2
+        self.updateTimer = 0
+
+        self.maxCurrentBullets = 3
+        
+    '''
+    Player Movement functions
+    '''
     def moveUp(self):
-        if self.rect.y > 0:
-            self.rect.y -= 1        
-    def moveDown(self):
+        self.rect.y -= 1
+            
+    def moveDown(self):  
         if self.rect.y < DISPLAY_HEIGHT_GAMEZONE - self.rect.height:
             self.rect.y += 1
+            
     def moveRight(self):
         if self.rect.x < DISPLAY_WIDTH_GAMEZONE - self.rect.width:
             self.rect.x += 1
+        
     def moveLeft(self):
-        if self.rect.x > 0:
-            self.rect.x -= 1
-            
+        self.rect.x -= 1            
+
+'''
+Class representing the player bullets.
+'''
 class PlayerBullet (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("bullet.png").convert()
         self.rect = self.image.get_rect()
-        self.speed_y = 5
 
-    def update(self):
-        self.rect.y -= self.speed_y
+        self.updateTimerLimit = 1
+        self.updateTimer = 0
+
+    def update(self, deltaTime):
+        if self.updateTimer >= self.updateTimerLimit:
+            self.rect.y -= 1
+            self.updateTimer = 0
+        self.updateTimer += 1
 
     def mustDie(self):
-        if self.rect.y < 0:
+        if self.rect.y <= 0:
             return True
         else:
             return False
