88efe3ce18a86f4d70c0acc712e16aa897814284
FranciscoAB <37817729+FranciscoAB@users.noreply.github.com>
2019-01-09 20:50:32 -0300

    Add files via upload

--- GameDisplay.py
+++ GameDisplay.py
@@ -5,7 +5,7 @@ class GameDisplay:
     def __init__(self, win):
 
         self.rect = pygame.draw.rect(win, (0, 0, 0), (0, 0, 0, 0))
-
+	print ("ah re")
         self.color = (255, 255, 255)
 
         self.rect.w = DISPLAY_WIDTH_TOTAL - DISPLAY_WIDTH_GAMEZONE
--- moh.py
+++ moh.py
@@ -12,6 +12,8 @@ from Util import *
 
 from Score import *
 
+print ("pedo")
+
 '''
 Initialization
 '''
